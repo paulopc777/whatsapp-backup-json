c0127cfcab9aa5107ee9d7088adef26488225cab
Paulo <paulinho.pc@outlook.com>
2024-11-14 17:43:09 -0300

    Update

--- main.ts
+++ main.ts
@@ -8,15 +8,30 @@ const FormatMessages = (msg: WAWebJS.Message[]) => {
   return msg.map((m) => ({ from: m.from, fromMe: m.fromMe, body: m.body }));
 };
 
-export async function main() {
-  const chats = (await client.getChats()).slice(0,10);
+async function GetChatsMax(MaxContacts: number, MaxMessages: number) {
+  const chats = (await client.getChats()).slice(0, MaxContacts);
 
   const d = await Promise.all(
     chats.map(async (c) => {
-      const msg = await c.fetchMessages({ limit: 10 });
+      const msg = await c.fetchMessages({ limit: MaxMessages });
       return { id: c.id.user, mensagens: FormatMessages(msg) };
     })
   );
-  saveJsonToFile(d)
+  saveJsonToFile(d);
   return d;
+}
+
+async function GetChatsByPhone(Phone: string, MaxMessages: number) {
+  const chat = (await client.getChats()).find((c) => c.id.user === Phone);
+  if (chat) {
+    const msg = await chat.fetchMessages({ limit: MaxMessages });
+    return { id: chat.id.user, mensagens: FormatMessages(msg) };
+  }
+  console.error(`Not find chat tho number ${Phone}`)
+  return false
+}
+
+export async function main() {
+  const d = await GetChatsMax(30,100);
+  if(d)saveJsonToFile(d);
 }
